73c701859b865deed4ec254ff109cddb26245d1e
Fleur41 <Muriithikings456@gmail.com>
2023-04-17 14:53:13 +0300

    Updated my Who We Are

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+import WhoWeAre from "./components/WhoWeAre";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className=" App">
+     <NavBar/>
+     <WhoWeAre/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
